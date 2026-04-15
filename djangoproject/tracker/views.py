2c2547413829c317e57e0fa1458ebd9bceabc85b
IsmaelBrea <ismabrearias@gmail.com>
2026-04-15 19:56:07 +0200

    Funcionalidades 2 y 3 y su vista de la pantalla home

--- djangoproject/tracker/views.py
+++ djangoproject/tracker/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from .services import fetch_competitions
+from .services import fetch_competitions, fetch_teams
 
 class LeaguesView(generic.TemplateView):
     template_name = "tracker/leagues.html"
@@ -7,19 +7,15 @@ class LeaguesView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        # 1. Obtenemos todas las competiciones de la API [cite: 16, 17]
+        # 1. Obtenemos competiciones
         all_leagues = fetch_competitions()
         
-        # 2. Definimos los IDs de las competiciones según el anteproyecto 
-        # Champions (2001), Mundial (2000), Premier (2021), LaLiga (2014), 
-        # Serie A (2019), Bundesliga (2002), Ligue 1 (2015)
+        # 2. IDs del anteproyecto 
         target_ids = [2001, 2000, 2021, 2014, 2019, 2002, 2015]
-        
-        # 3. Filtramos para quedarnos solo con las elegidas [cite: 17, 18]
         leagues = [league for league in all_leagues if league['id'] in target_ids]
         context['leagues'] = leagues
         
-        # 4. Gestionamos la liga seleccionada por el usuario 
+        # 3. Gestionamos selección (Funcionalidad F1) 
         selected_league_id = self.request.GET.get('league')
         selected_league = None
         
@@ -28,6 +24,9 @@ class LeaguesView(generic.TemplateView):
                 if str(league['id']) == selected_league_id:
                     selected_league = league
                     break
+            
+            # 4. Si hay liga, traemos equipos (Funcionalidad F2) 
+            context['teams'] = fetch_teams(selected_league_id)
         
         context['selected_league'] = selected_league
         return context
