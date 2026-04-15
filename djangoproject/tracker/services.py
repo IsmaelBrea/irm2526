cd95adbbd1051175fb02e5c061c79a9d85964bfb
rubenuzuu <ruben.gonzalez.ouzounis@udc.es>
2026-04-15 13:18:28 +0200

    Limpieza tutorial

--- djangoproject/tracker/services.py
+++ djangoproject/tracker/services.py
@@ -16,28 +16,6 @@ class APITokenPool:
 # API Keys
 TOKEN_LIST = ["6728e2d7b5c06562344b779385eeed84", "096e2a18cca2f5be6feaa985b556f679"]
 token_pool = APITokenPool(TOKEN_LIST)
-
-def fetch_team_stats(team_id, league=39, season=2023):
-    """
-    F13: Obtención de datos desde API-Sports (Directa).
-    """
-
-    url = "https://v3.football.api-sports.io/teams/statistics"
-    
-    headers = {
-        "x-apisports-key": token_pool.get_token() # Cabecera correcta para esta URL
-    }
-    
-    params = {"league": league, "season": season, "team": team_id}
-
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        print(f"DEBUG: Probando conexión directa. Status: {response.status_code}")
-        response.raise_for_status()
-        return response.json()
-    except Exception as e:
-        print(f"Error en la conexión directa: {e}")
-        return None
     
 
 def fetch_competitions():
