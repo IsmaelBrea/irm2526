dcac5368cfd41aa3959f2eb7ad9857354b4baa4c
IsmaelBrea <ismabrearias@gmail.com>
2026-04-16 20:05:25 +0200

    fix: reorganizar estructura de plantillas en base, index y leagues

--- djangoproject/tracker/views.py
+++ djangoproject/tracker/views.py
@@ -4,20 +4,20 @@ from .services import fetch_competitions, fetch_teams
 from .services import fetch_competitions, fetch_scorers
 
 class LeaguesView(generic.TemplateView):
-    template_name = "tracker/leagues.html"
+    template_name = "tracker/index.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        # 1. Obtenemos competiciones
+        # Obtenemos competiciones
         all_leagues = fetch_competitions()
         
-        # 2. IDs del anteproyecto 
+        # IDs del anteproyecto 
         target_ids = [2001, 2000, 2021, 2014, 2019, 2002, 2015]
         leagues = [league for league in all_leagues if league['id'] in target_ids]
         context['leagues'] = leagues
         
-        # 3. Gestionamos selección (Funcionalidad F1) 
+        # Gestionamos selección (Funcionalidad F1) 
         selected_league_id = self.request.GET.get('league')
         selected_league = None
         scorers = []
@@ -32,7 +32,7 @@ class LeaguesView(generic.TemplateView):
 
                     break
             
-            # 4. Si hay liga, traemos equipos (Funcionalidad F2) 
+            # Si hay liga, traemos equipos (Funcionalidad F2) 
             context['teams'] = fetch_teams(selected_league_id)
         
         context['selected_league'] = selected_league
